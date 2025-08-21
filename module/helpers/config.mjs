75d713b407520a69e539676362eccd5839c8852a
Ryan Green <ryangreen062@gmail.com>
2025-08-21 12:58:08 -0400

    retry first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TRINITY_SECOND = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+TRINITY_SECOND.abilities = {
+  str: 'TRINITY_SECOND.Ability.Str.long',
+  dex: 'TRINITY_SECOND.Ability.Dex.long',
+  con: 'TRINITY_SECOND.Ability.Con.long',
+  int: 'TRINITY_SECOND.Ability.Int.long',
+  wis: 'TRINITY_SECOND.Ability.Wis.long',
+  cha: 'TRINITY_SECOND.Ability.Cha.long',
+};
+
+TRINITY_SECOND.abilityAbbreviations = {
+  str: 'TRINITY_SECOND.Ability.Str.abbr',
+  dex: 'TRINITY_SECOND.Ability.Dex.abbr',
+  con: 'TRINITY_SECOND.Ability.Con.abbr',
+  int: 'TRINITY_SECOND.Ability.Int.abbr',
+  wis: 'TRINITY_SECOND.Ability.Wis.abbr',
+  cha: 'TRINITY_SECOND.Ability.Cha.abbr',
+};
---
+++ module/helpers/templates.mjs
@@ -0,0 +1,16 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
+export const preloadHandlebarsTemplates = async function () {
+  return loadTemplates([
+    // Actor partials.
+    'systems/trinity-2e/templates/actor/parts/actor-features.hbs',
+    'systems/trinity-2e/templates/actor/parts/actor-items.hbs',
+    'systems/trinity-2e/templates/actor/parts/actor-spells.hbs',
+    'systems/trinity-2e/templates/actor/parts/actor-effects.hbs',
+    // Item partials
+    'systems/trinity-2e/templates/item/parts/item-effects.hbs',
+  ]);
+};
